aa90edbe39f318f7721f421b24a87718b69fadb7
Sinisa Damjanovic <sikidamjanovic@gmail.com>
2019-06-12 18:16:06 -0400

    Hook buttons up to Amazon links - Final commit

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,18 +1,79 @@
 import React, {Component} from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image, Linking } from 'react-native';
 import Card from './Card';
 import CardSection from './CardSection';
+import Button from './Button'
 
-class AlbumDetails extends Component{
-    render(props){
-        return(
-            <Card>
-                <CardSection>
-                    <Text>{this.props.album.title}</Text>
-                </CardSection>
-            </Card>
-        )
-    };
+const AlbumDetails = ({ album }) => {
+
+    const { title, 
+            artist, 
+            thumbnail_image, 
+            image,
+            url 
+        } = album;
+
+    const { thumbnailStyle, 
+            headerContentStyle,
+            thumbnailContainerStyle,
+            headerTextStyle,
+            imageStyle
+        } = styles;
+
+    return(
+        <Card>
+            {/* Artist details + image*/}
+            <CardSection>
+                <View style = { thumbnailContainerStyle }>
+                    <Image
+                        style = { thumbnailStyle } 
+                        source = {{ uri: thumbnail_image }} 
+                    />
+                </View>
+                <View style = { headerContentStyle }>
+                    <Text style = { headerTextStyle }>{ title }</Text>
+                    <Text>{ artist }</Text>
+                </View>
+            </CardSection>
+            {/* Album Cover*/}
+            <CardSection>
+                <Image 
+                    style = { imageStyle }
+                    source = {{ uri: image }}/>
+            </CardSection>
+            {/* Buy Button */}
+            <CardSection>
+                <Button onPress={() => Linking.openURL(url)} album = { title }/>
+            </CardSection>
+
+        </Card>
+    )
 };
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    headerTextStyle: {
+        fontSize: 18,
+        fontWeight: 'bold'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
+    }
+}
 
 export default AlbumDetails;
